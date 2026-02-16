1f04408395be6ca5d13209f5f8914b7dff8a4f0c
alebinson <newdarkvirus@gmail.com>
2026-02-16 14:32:35 +0200

    fix: cesium and ol synch and more

---
+++ src/common/icons/ExclamationSVGIcon.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { SVGIconProps } from './SVGIconProps';
+
+export const ExclamationSVGIcon: React.FC<SVGIconProps> = ({ color, className }) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" 
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke={color}
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className={className}
+    >
+      <path d="m21.73 18-8-14a2 2 0 0 0-3.48 0l-8 14A2 2 0 0 0 4 21h16a2 2 0 0 0 1.73-3"></path>
+      <path d="M12 9v4"></path>
+      <path d="M12 17h.01"></path>
+    </svg>
+  );
+};
