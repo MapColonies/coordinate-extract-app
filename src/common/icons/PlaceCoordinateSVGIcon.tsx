15d8556105c1a48550380130ae647bbe26ab3260
Eliav Maimon <liavma3@gmail.com>
2026-02-23 14:50:05 +0200

    fix: pr

--- src/common/icons/PlaceCoordinateSVGIcon.tsx
+++ src/common/icons/PlaceCoordinateSVGIcon.tsx
@@ -4,18 +4,18 @@ import { SVGIconProps } from './SVGIconProps';
 export const PlaceCoordinateSVGIcon: React.FC<SVGIconProps> = ({ color, className }) => {
   return (
     <svg width="40" height="40" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="none">
-      <circle cx="12" cy="12" r="9" stroke={color} stroke-width="1.4" />
-      <path d="M3 12h18" stroke={color} stroke-width="1" stroke-linecap="round" opacity="0.7" />
-      <path d="M6 8c2.5 1.6 9.5 1.6 12 0" stroke={color} stroke-width="1" stroke-linecap="round" opacity="0.6" />
-      <path d="M6 16c2.5-1.6 9.5-1.6 12 0" stroke={color} stroke-width="1" stroke-linecap="round" opacity="0.6" />
-      <path d="M12 3c-1.5 3-1.5 15 0 18" stroke={color} stroke-width="1" stroke-linecap="round" opacity="0.6" />
+      <circle cx="12" cy="12" r="9" stroke={color} strokeWidth="1.4" />
+      <path d="M3 12h18" stroke={color} strokeWidth="1" strokeLinecap="round" opacity="0.7" />
+      <path d="M6 8c2.5 1.6 9.5 1.6 12 0" stroke={color} strokeWidth="1" strokeLinecap="round" opacity="0.6" />
+      <path d="M6 16c2.5-1.6 9.5-1.6 12 0" stroke={color} strokeWidth="1" strokeLinecap="round" opacity="0.6" />
+      <path d="M12 3c-1.5 3-1.5 15 0 18" stroke={color} strokeWidth="1" strokeLinecap="round" opacity="0.6" />
       <path
         d="M16 8
            a2 2 0 1 0 -4 0
            c0 1.8 2 4 2 4
            s2-2.2 2-4z"
         stroke={color}
-        stroke-width="1.3"
+        strokeWidth="1.3"
         fill="none"
         className={className}
       />
